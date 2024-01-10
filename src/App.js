0730933ff13f3f4c172f0f58b1c66b8a00ca5e0c
manya706 <sharma.manya721@gmail.com>
2024-01-10 14:25:59 +0530

    created bootstraped table for all stock related things

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import StockRow from './components/StockRow';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <table className="table mt-5">
+          <thead>
+            <tr>
+              <th>Ticker</th>
+              <th>Price</th>
+              <th>Date</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            <StockRow ticker="apple"/>
+            <StockRow ticker="Google"/>
+            <StockRow ticker="Microsoft"/>
+            <StockRow ticker="Tesla"/>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
